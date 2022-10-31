c6a66b9e6fe7c913737ddda26066170d08426dba
Hajime Fujita <hajime.fujita@gmail.com>
2022-10-31 21:54:26 +0900

    いくつかの問題を解いた

---
+++ 20221031_ABC275_B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  long long A, B, C, D, E, F;
+  cin >> A >> B >> C >> D >> E >> F;
+
+  cout << (A * B * C - D * E * F) % 998244353 << endl;
+  
+  return 0;
+  
+}
+
+// cin >> N >> X;
+// for (int i = 0; i < N; i++)  cin >> A[i];
+
+// int pos = lower_bound(A + 1, A + (N + 1), X[j]) - A;  // 二分探索
+
+// sort(A, A + N);  // 昇順ソート
+// sort(A, A + N, greater<>());  // 降順ソート
+
+// vector<int> vec(N);  // ベクターの宣言（１次元）
+
+// vector< vector<int> > a(N);  // ベクターの宣言（２次元）
+// a[i] = vector<int>(L);
